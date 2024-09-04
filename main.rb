e5edbbd0be81cd6fa7f4fc6b377faf4568a3c33d
Loren Segal <lsegal@soen.ca>
2024-09-04 02:01:29 -0700

    Support up to level 5 headings
    Fixes #1

--- main.rb
+++ main.rb
@@ -22,7 +22,7 @@ filenames = (ENV['INPUT_FILENAME'] || '').empty? ?
 
 # Extract changelog portion
 version = tag.sub(/^v/, '')
-re = %r{^\#{1,2}\s+\[?v?#{Regexp.quote version}[^\r\n]*\r?\n(.+?)(^#|\Z)}mi
+re = %r{^\#{1,5}\s+\[?v?#{Regexp.quote version}[^\r\n]*\r?\n(.+?)(^#|\Z)}mi
 filename = Dir['*'].find {|f| filenames.include?(f.downcase) }
 raise "Cannot find any valid changelog files from #{filenames.inspect}" if filename.nil?
 extracted = File.read(filename)[re, 1]
